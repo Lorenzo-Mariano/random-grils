63359c8869a7ad3446e7b984741db4884b4cc536
Lorenzo-Mariano <hhumminah@gmail.com>
2024-06-22 20:19:02 +0800

    add query string to blob url's

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,25 @@ import Image from "next/image";
 
 export default async function Page() {
 	const grils = await list();
-	console.log(grils);
+	const key = randomUUID().toString();
+	console.log(key);
 
 	return (
 		<>
-			{grils &&
-				grils.blobs.map((image) => {
-					return (
-						<Image
-							key={randomUUID().toString()}
-							alt={image.pathname}
-							src={image.url}
-							height={400}
-							width={400}
-						/>
-					);
-				})}
+			<section className="flex flex-wrap justify-center gap-4">
+				{grils &&
+					grils.blobs.map((image) => {
+						return (
+							<Image
+								key={randomUUID().toString()}
+								alt={image.pathname}
+								src={`${image.url}?cache-key=${key}`}
+								height={400}
+								width={400}
+							/>
+						);
+					})}
+			</section>
 		</>
 	);
 }
