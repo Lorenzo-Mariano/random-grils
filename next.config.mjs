c313ec2e13e320f7db596d9beb52ce0fa4b3952b
Lorenzo-Mariano <hhumminah@gmail.com>
2024-06-22 19:09:05 +0800

    Can now view uploaded grils!

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: "https",
+				hostname: "pdcyuo0zxamluo0c.public.blob.vercel-storage.com",
+			},
+			{
+				protocol: "https",
 				hostname: "cdn.dummyjson.com",
 				port: "",
 				pathname: "/recipe-images/**",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-	return <span>paeg</span>;
+import { list } from "@vercel/blob";
+import { randomUUID } from "crypto";
+import Image from "next/image";
+
+export default async function Page() {
+	const grils = await list();
+	console.log(grils);
+
+	return (
+		<>
+			{grils &&
+				grils.blobs.map((image) => {
+					return (
+						<Image
+							key={randomUUID().toString()}
+							alt={image.pathname}
+							src={image.url}
+							height={400}
+							width={400}
+						/>
+					);
+				})}
+		</>
+	);
 }
